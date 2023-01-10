1a13d821aefbb9efac0660b204fbc0de9e7cb19f
Lalessio <catrini.alessio2@gmail.com>
2023-01-10 23:43:19 +0100

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from "./components/HomeScreen";
+import AboutScreen from "./components/AboutScreen";
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    const Tab = createBottomTabNavigator();
+    return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Telecamere" component={HomeScreen} />
+        <Tab.Screen name="Valvole" component={AboutScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
